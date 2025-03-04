a37e828c8134737dfc6309a11cdd66ad9ec9221c
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-03-03 23:29:28 -0600

    Se agrego que el metodo para ver las facturas de un usuario , solo los admins puedan hacerlo

--- src/invoice/invoice.routes.js
+++ src/invoice/invoice.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { getInvoicesByUser } from "./invoice.controller.js";
 import { validateJwt } from "../../middelwares/validate.jwt.js";
-import { validateAuthenticUserParam } from "../../middelwares/validate.user.js";
+import { validateAuthenticUserParam, validateCredentialsAsAdmin } from "../../middelwares/validate.user.js";
 
 const apiInvoice = Router()
 
-apiInvoice.get('/invoice-user/:userId',validateJwt,validateAuthenticUserParam,getInvoicesByUser)
+apiInvoice.get('/invoice-user/:userId',validateJwt,validateCredentialsAsAdmin,validateAuthenticUserParam,getInvoicesByUser)
 
 export default apiInvoice
